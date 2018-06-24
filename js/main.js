dc05316bdfe42f304c5a29dcaf570d9fe92a40d0
Larisse Évelyn <larissebcarvalho@gmail.com>
2018-06-24 01:42:22 -0300

    #02: Filtra por classe

---
+++ js/main.js
@@ -0,0 +1,5 @@
+var titulo = document.querySelector(".titulo");
+//console.log(titulo);
+//console.log(titulo.textContent);
+console.log("fui carregado de um arquivo externo");
+titulo.textContent = "COisa";
