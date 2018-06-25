6290e561df0ca5fcff1725bd30b689910eabd758
Larisse Évelyn <larissebcarvalho@gmail.com>
2018-06-25 17:15:54 -0300

    #03: Insere operadores lógicos e validação

--- js/main.js
+++ js/main.js
@@ -1,5 +1,37 @@
 var titulo = document.querySelector(".titulo");
-//console.log(titulo);
-//console.log(titulo.textContent);
-console.log("fui carregado de um arquivo externo");
-titulo.textContent = "COisa";
+titulo.textContent = "Aparecida Nutricionista";
+
+var paciente = document.querySelector("#primeiro-paciente");
+
+tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+tdImc = paciente.querySelector(".info-imc");
+
+var pesovalido = true;
+var alturavalida = true;
+
+if (peso <= 0 || peso >= 1000) {
+  console.log("Peso inválido");
+  pesovalido = false;
+  tdImc.textContent = "Peso inválido";
+}
+
+if(altura <= 0 || altura >= 3){
+  console.log("Altura inválida");
+  alturavalida = false;
+  tdImc.textContent = "Altura inválida";
+}
+
+if(pesovalido && alturavalida){
+  var imc = peso/(altura*altura);
+  tdImc.textContent = imc;  
+}
+
+
+
+
+
