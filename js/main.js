ae32a2dc8f740e4c490542ac96c63c3fab9eec5c
Larisse Évelyn <larissebcarvalho@gmail.com>
2018-06-25 17:35:39 -0300

    #04: Trabalha estilos em JS

--- js/main.js
+++ js/main.js
@@ -1,35 +1,47 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
-var paciente = document.querySelector("#primeiro-paciente");
+var pacientes = document.querySelectorAll(".paciente");
 
-tdPeso = paciente.querySelector(".info-peso");
-var peso = tdPeso.textContent;
+for(var i = 0; i < pacientes.length; i ++){
+  
+  var paciente = pacientes[i];
+  
+  tdPeso = paciente.querySelector(".info-peso");
+  var peso = tdPeso.textContent;
 
-tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
+  tdAltura = paciente.querySelector(".info-altura");
+  var altura = tdAltura.textContent;
 
-tdImc = paciente.querySelector(".info-imc");
+  tdImc = paciente.querySelector(".info-imc");
 
-var pesovalido = true;
-var alturavalida = true;
+  var pesovalido = true;
+  var alturavalida = true;
 
-if (peso <= 0 || peso >= 1000) {
-  console.log("Peso inválido");
-  pesovalido = false;
-  tdImc.textContent = "Peso inválido";
+  if (peso <= 0 || peso >= 1000) {
+    console.log("Peso inválido");
+    pesovalido = false;
+    tdImc.textContent = "Peso inválido";
+    paciente.classList.add("paciente-invalido");
+  }
+
+  if(altura <= 0 || altura >= 3){
+    console.log("Altura inválida");
+    alturavalida = false;
+    tdImc.textContent = "Altura inválida";
+    paciente.classList.add("paciente-invalido");
+  }
+
+  if(pesovalido && alturavalida){
+    var imc = peso/(altura*altura);
+    tdImc.textContent = imc.toFixed(2);  
+  }
+  
+  
 }
 
-if(altura <= 0 || altura >= 3){
-  console.log("Altura inválida");
-  alturavalida = false;
-  tdImc.textContent = "Altura inválida";
-}
 
-if(pesovalido && alturavalida){
-  var imc = peso/(altura*altura);
-  tdImc.textContent = imc;  
-}
+
 
 
 
